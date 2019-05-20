ddabe32bfafd3ae9a50abe3a99a4c2164832bee1
dkozinn <david@kozinn.com>
2019-05-20 00:26:22 +0000

    Removed dummy comment

--- ns.pl
+++ ns.pl
@@ -2,7 +2,6 @@
 
 # Hacked from the sample code at http://www.net-dns.org/docs/Net/DNS/Nameserver.html
 # Now writes tracing output to syslog using local0.info
-# Dummy add to test git 
  
 use strict;
 use warnings;
